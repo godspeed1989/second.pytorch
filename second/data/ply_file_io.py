1725e7304697aba71085d1b97b4c019ac025f0b0
godspeed1989 <tongxinjichu@gmail.com>
2018-11-08 17:02:36 +0800

    ply file io

---
+++ second/data/ply_file_io.py
@@ -0,0 +1,17 @@
+import numpy as np
+from second.data.ply_file_internal import PlyData, PlyElement
+
+def read_ply(filename):
+    """ read XYZ point cloud from filename PLY file """
+    plydata = PlyData.read(filename)
+    pc = plydata['vertex'].data
+    pc_array = np.array([[x, y, z] for x,y,z in pc])
+    return pc_array
+
+
+def write_ply(points, filename, text=True):
+    """ input: Nx3, write points to filename as PLY format. """
+    points = [(points[i,0], points[i,1], points[i,2]) for i in range(points.shape[0])]
+    vertex = np.array(points, dtype=[('x', 'f4'), ('y', 'f4'),('z', 'f4')])
+    el = PlyElement.describe(vertex, 'vertex', comments=['vertices'])
+    PlyData([el], text=text).write(filename)
